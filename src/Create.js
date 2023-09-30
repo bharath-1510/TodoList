1588eca308ec6681de26701b597685bbb2e44a29
Bharath <89100001+bharath-1510@users.noreply.github.com>
2023-09-30 13:10:31 +0530

    Add a Task

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const completed = false;
+  const [IsPending, SetIsPending] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    SetIsPending(true);
+    const task = { title, completed };
+    fetch("http://localhost:8000/tasks", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(task),
+    }).then(() => {
+      SetIsPending(false);
+    });
+  };
   return (
     <div className="create">
       <h2>Add New Task</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Task:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        ></input>
+        {!IsPending && <button>Add Task</button>}
+        {IsPending && <button disabled>Adding</button>}
+      </form>
     </div>
   );
 };
